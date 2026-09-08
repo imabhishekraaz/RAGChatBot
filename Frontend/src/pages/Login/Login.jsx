352c3a42ab741597b7065bdcc7f32b4e2412c7d5
imabhishekraaz <ffabhishek116@gmail.com>
2026-09-08 22:56:34 +0530

    updates the Login page and README.md file

---
+++ Frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login =()=> {
+    return (
+        <>
+            <div>
+                <div>
+                    <input type="text" />
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Login;
